e8f84c8025d603d3c1b6a920417a0aaa72ad523a
민유성 <ysmin@mittelstand.co.kr>
2015-02-06 18:35:07 +0900

    css 수정 및 푸터수정

--- inc/footer/globalFooter.php
+++ inc/footer/globalFooter.php
@@ -3,8 +3,9 @@
 			$("img").removeAttr("title");
 			</script>
 			<div style="height:80px; clear:both;"></div>
-			<div id="footer" style="width:720px; line-height:30px;clear:both; position:absolute; bottom:20px; left:50%; margin-left:-465px" >	
-				<span class="copyRight" style="color:#FFF; margin:0 auto; display:block;">
+			<div><idv>
+			<div id="footer" style="width:100%; line-height:30px;clear:both; position:relative; bottom:20px; left:50%; margin-left:-465px; background-color:#0198f1" >	
+				<span class="copyRight" style="width:720px; color:#FFF; margin:0 auto; position:absolute; display:block; left:50%; margin-left:-465px">
 					ⓒ 2015 Mittelstand Co., Ltd. <a style="margin:0 20px" >회사소개</a> <a style="margin:0 20px 0 0 " href="/terms" >이용약관</a> <a style="margin:0 20px 0 0" href="/policy" >개인정보취급방침</a>
 				</span>
 			</div>

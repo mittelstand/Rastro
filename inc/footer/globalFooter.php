6a072892f58c6a7a6f831b63612061c570c08bc3
민유성 <ysmin@mittelstand.co.kr>
2015-02-06 18:39:33 +0900

    test

--- inc/footer/globalFooter.php
+++ inc/footer/globalFooter.php
@@ -3,6 +3,7 @@
 			$("img").removeAttr("title");
 			</script>
 			<div style="height:80px; clear:both;"></div>
+			<div style="width:100%; line-height:30px; clear:both; position:absolute; bottom:20px; background-color:#0198f1;"></div>
 			<div id="footer" style="width:720px; line-height:30px;clear:both; position:absolute; bottom:20px; left:50%; margin-left:-465px" >	
 				<span class="copyRight" style="color:#FFF; margin:0 auto; display:block;">
 					ⓒ 2015 Mittelstand Co., Ltd. <a style="margin:0 20px" >회사소개</a> <a style="margin:0 20px 0 0 " href="/terms" >이용약관</a> <a style="margin:0 20px 0 0" href="/policy" >개인정보취급방침</a>

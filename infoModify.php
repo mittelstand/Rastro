3260a467dad09db0274eccc56c091464439b8d19
박유경 <ygpark@mittelstand.co.kr>
2015-02-06 21:20:13 +0900

    ㅍㅂ ㅂㄷㅂㄷ

--- infoModify.php
+++ infoModify.php
@@ -15,10 +15,10 @@
 		$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$newName."'";
 		move_uploaded_file($_FILES['picture']['tmp_name'], $newName);
 	}else{
-		if($_POST["fbChange"]){
-			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$_POST["fbChange"]."'";
-		}else{
+		if($_POST["fbChange"] == "" ){
 			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."'";
+		}else{
+			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$_POST["fbChange"]."'";
 		}
 	}
 	

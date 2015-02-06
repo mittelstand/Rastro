361a9d27cd9599e61ef38fd86d9cb831c4123ec0
민유성 <ysmin@mittelstand.co.kr>
2015-02-06 18:36:03 +0900

    ㅍㅍㅍ

--- inc/footer/globalFooter.php
+++ inc/footer/globalFooter.php
@@ -4,7 +4,7 @@
 			</script>
 			<div style="height:80px; clear:both;"></div>
 			<div><idv>
-			<div id="footer" style="width:100%; line-height:30px;clear:both; position:relative; bottom:20px; left:50%; margin-left:-465px; background-color:#0198f1" >	
+			<div id="footer" style="width:100%; line-height:30px;clear:both; position:absolute; bottom:20px; left:50%; margin-left:-465px; background-color:#0198f1" >	
 				<span class="copyRight" style="width:720px; color:#FFF; margin:0 auto; position:absolute; display:block; left:50%; margin-left:-465px">
 					ⓒ 2015 Mittelstand Co., Ltd. <a style="margin:0 20px" >회사소개</a> <a style="margin:0 20px 0 0 " href="/terms" >이용약관</a> <a style="margin:0 20px 0 0" href="/policy" >개인정보취급방침</a>
 				</span>

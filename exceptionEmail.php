3030dc71881e94bb207d09f977b9c203148c51d8
민유성 <ysmin@mittelstand.co.kr>
2015-02-09 16:24:12 +0900

    유경씨 선물2

--- exceptionEmail.php
+++ exceptionEmail.php
@@ -5,6 +5,9 @@ $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/mainHeader.php";
 ?>
 <form method="post" action="/joinInsert.php" id="joinForm">
+	<div class="leftObj">		
+        <span class="text3">이메일 입력하라고 진짜 ㅋㅋㅋㅋ</span>
+	</div>
 	<ul class="joinForm">
 		<li class="email list">
 			<!--span class="lab">E-mail</span-->

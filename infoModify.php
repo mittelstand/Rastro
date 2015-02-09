3577fc95136478f3ba470a8af526b2dfe95fed90
박유경 <ygpark@mittelstand.co.kr>
2015-02-09 11:23:53 +0900

    ㅍㅂ

--- infoModify.php
+++ infoModify.php
@@ -8,22 +8,23 @@
 	$birth = $_POST["birthYear"]."-".$_POST["birthMonth"]."-".$_POST["birthDay"];
 	$uploadDir = $dir."/file/";
 
-	if($_FILES["picture"]){
+	if($_FILES["picture"]["name"]){
 
-		if($_POST["fbChange"] != ""){
-			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$_POST["fbChange"]."'";
-		}else{
-			$exp = explode(".",$_FILES["picture"]["name"]);
-			$newName = $uploadDir.time().$_SESSION["idx"].".".$exp[1];		
-			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$newName."'";
-		}
+		$exp = explode(".",$_FILES["picture"]["name"]);
+		$newName = $uploadDir.time().$_SESSION["idx"].".".$exp[1];		
+		$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$newName."'";
+		
 		move_uploaded_file($_FILES['picture']['tmp_name'], $newName);
 	}else{
-		$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."'";
+		if($_POST["fbChange"]){
+			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$_POST["fbChange"]."'";
+		}else{
+			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."'";
+		}
 	}
 	$db->where = "idx=".$_SESSION['idx'];	
 	$db->Update();
 ?>
 <Script>
-	location.href="info.php";
+	location.href="info";
 </script>

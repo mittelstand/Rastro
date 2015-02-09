e948e752f60415a9321841dc3507e1f620b68a56
박유경 <ygpark@mittelstand.co.kr>
2015-02-09 09:07:14 +0900

    ㅍㅂ

--- infoModify.php
+++ infoModify.php
@@ -8,8 +8,21 @@
 	$birth = $_POST["birthYear"]."-".$_POST["birthMonth"]."-".$_POST["birthDay"];
 	$uploadDir = $dir."/file/";
 
-	echo $_FILES["picture"]["name"];
-	echo $_POST["fbChange"];
+	if($_FILES["picture"]){
+			$exp = explode(".",$_FILES["picture"]["name"]);
+			$newName = $uploadDir.time().$_SESSION["idx"].".".$exp[1];		
+			$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$newName."'";
+		move_uploaded_file($_FILES['picture']['tmp_name'], $newName);
 
 
+		echo $_POST["fbChange"]." 있어욥";
+	}else{
+		$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."'";
+		echo $_POST["fbChange"]." 없어욥";
+	}
+	$db->where = "idx=".$_SESSION['idx'];	
+	$db->Update();
 ?>
+<Script>
+	location.href="info.php";
+</script>

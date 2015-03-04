da1df65099f63bd7386b2229ca0a594b00d50c30
민유성 <ysmin@mittelstand.co.kr>
2015-03-04 14:47:01 +0900

    fgfgfgfgfg

--- infoModify.php
+++ infoModify.php
@@ -13,7 +13,7 @@
 		$exp = explode(".",$_FILES["picture"]["name"]);
 		$newName = $uploadDir.time().$_SESSION["idx"].".".$exp[1];		
 		$db->field = "email = '".$_POST["email"]."', name='".$_POST["name"]."', dob='".$birth."', sex='".$_POST["sex"]."', Ps='".$newName."'";
-		echo $db->field;
+		
 		move_uploaded_file($_FILES['picture']['tmp_name'], $newName);
 	}else{
 		if($_POST["fbChange"]){
@@ -25,6 +25,7 @@
 		}
 	}
 	$db->where = "idx='".$_SESSION['idx']."'";	
+	echo $db->where;
 	$db->Update();
 ?>
 <Script>

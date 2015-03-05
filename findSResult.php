4d31f4f6fba8c5d4e4ee8cfbb069141d6545e5c0
민유성 <ysmin@mittelstand.co.kr>
2015-03-05 21:10:21 +0900

    마무리

--- findSResult.php
+++ findSResult.php
@@ -4,8 +4,8 @@ $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/mainHeader.php";
 
 ?>
-<div class = "message">임시비밀번호가 발급되었습니다. 로그인 하시겠습니까?</div>
-
+<div class = "messageFind">임시비밀번호가 발급되었습니다.<br/> 로그인 하시겠습니까?</div>
+<button type="submit" class="btnLogin">로그인</button>
 
 <?
 include $dir."/inc/footer/mainFooter.php";

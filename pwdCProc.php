cd2a82f39eb7e01e6f01fc2e0cce66e70f29aa4c
민유성 <ysmin@mittelstand.co.kr>
2015-03-06 14:02:20 +0900

    test

--- pwdCProc.php
+++ pwdCProc.php
@@ -5,21 +5,28 @@ include $dir."/inc/header/inc.php";
 $db = new Dbcon();
 $db->table = "member";
 $db->keyfield = "idx";
-$db->field = "fbcode";
 $db->where = "idx = '".$_SESSION['idx']."' and ((pwd = '".md5($_POST['nowPwd'])."') or (pwdTemp='".$_POST['nowPwd']."'))";
-$row = mysql_fetch_array($db->Select());
-echo $db->where;
-if(($db->TotalCnt() > 0) or ($row['fbcode'] > 0)){
+
+if(($db->TotalCnt() > 0)){
 	$db->field = "pwd='".md5($_POST['pwd'])."'";
 	$db->Update();
 }else{
-	//MsgBox("현재 비밀번호가 틀립니다.","back");
-	unset($db);
-	exit();
+	
+	$db->field = "fbcode";
+	$db->where = "idx = '".$_SESSION['idx']."'";
+	$row = mysql_fetch_array($db->Select());
+	if($row['fbcode'] > 0){
+		$db->field = "pwd='".md5($_POST['pwd'])."'";
+		$db->Update();
+	}else{
+		MsgBox("현재 비밀번호가 틀립니다.","back");
+		unset($db);
+		exit();
+	}
 }
 unset($db);
 ?>
 
 <script>
-	//location.href="info";
+	location.href="info";
 </script>

d94e958dda1a8132be33c24c8cd86daaf51a35c0
전우현 <jwh@mittelstand.co.kr>
2015-03-06 17:14:42 +0900

    제발되라

--- app/prizeInsert.php
+++ app/prizeInsert.php
@@ -6,14 +6,17 @@ include $dir."/inc/header/inc.php";
 $uploadDir = $dir."/file/";
 $fileName = $_POST['fName'];
 $uploadFile = $uploadDir.time().".jpg";
-echo $uploadFile;
+
+
 $db= new Dbcon();
 $db->table="prize";
 $db->field = "fidx,writer,pname,pinstitution,pdetail,src,cdate";
+if($_POST['idx']==""){
+}else{
 $db->value="'".$_POST['idx']."','".$_POST['name']."','".$_POST['Pname']."','".$_POST['Pinst']."','".$_POST['Pdetails']."','".$uploadFile."',now()";
 $i =$db->Insert();
-
-echo $i.",";
+echo $i;
 move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$uploadFile);
+}
 unset($db);
 ?>

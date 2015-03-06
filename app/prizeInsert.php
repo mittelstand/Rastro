0d62d221b4fa2646b3a1b0fb9ac338a31947d449
전우현 <jwh@mittelstand.co.kr>
2015-03-06 16:15:41 +0900

    사진첨부 - 미ㅌㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- app/prizeInsert.php
+++ app/prizeInsert.php
@@ -2,16 +2,18 @@
 header("Content-type: text/html; charset=utf-8"); 
 $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/inc.php";
-include $dir.'/gmail/MAIL.php';
-/*if($_POST['birthYear'] and $_POST['birthMonth'] and $_POST['birthDay']){
-	$_POST['dob'] = $_POST['birthYear']."-".$_POST['birthMonth']."-".$_POST['birthDay'];
-}*/
 
+$uploadDir = $dir."/file/";
+$fileName = $_POST['fName'];
+$uploadFile = $uploadDir.time().".jpg";
+echo ;
 $db= new Dbcon();
 $db->table="prize";
-$db->field = "fidx,writer,pname,pinstitution,pdetail";
-$db->value = 	"'".$_POST['idx']."','".$_POST['name']."','".$_POST['Pname']."','".$_POST['Pinst']."','".$_POST['Pdetails']."'";
+$db->field = "fidx,writer,pname,pinstitution,pdetail,src,cdate";
+$db->value="'".$_POST['idx']."','".$_POST['name']."','".$_POST['Pname']."','".$_POST['Pinst']."','".$_POST['Pdetails']."',".$uploadFile."',now";
 $i =$db->Insert();
-echo $i;
+
+echo $i.",".$uploadFile;
+move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$uploadFile);
 unset($db);
 ?>

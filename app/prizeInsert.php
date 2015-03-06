857ec228b57660622f8d7c874171aa8a6b23a58f
전우현 <jwh@mittelstand.co.kr>
2015-03-06 16:22:31 +0900

    흐흐흐흐흐흫 사진첨부

--- app/prizeInsert.php
+++ app/prizeInsert.php
@@ -6,11 +6,11 @@ include $dir."/inc/header/inc.php";
 $uploadDir = $dir."/file/";
 $fileName = $_POST['fName'];
 $uploadFile = $uploadDir.time().".jpg";
-echo ;
+echo $uploadFile;
 $db= new Dbcon();
 $db->table="prize";
 $db->field = "fidx,writer,pname,pinstitution,pdetail,src,cdate";
-$db->value="'".$_POST['idx']."','".$_POST['name']."','".$_POST['Pname']."','".$_POST['Pinst']."','".$_POST['Pdetails']."',".$uploadFile."',now";
+$db->value="'".$_POST['idx']."','".$_POST['name']."','".$_POST['Pname']."','".$_POST['Pinst']."','".$_POST['Pdetails']."','".$uploadFile."',now()";
 $i =$db->Insert();
 
 echo $i.",".$uploadFile;

ce2f6e246197e3f0a7244c04ef916b3dba9014a4
민유성 <ysmin@mittelstand.co.kr>
2015-03-06 10:02:26 +0900

    ㅇㅇ

--- findFResult.php
+++ findFResult.php
@@ -4,6 +4,7 @@ $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/mainHeader.php";
 
 ?>
+<div class = "messageFind">페이스북으로 가입하신 회원이십니다.<br/> 로그인 하시겠습니까?</div>
 <button type="button" class="btnFJoin" onclick="location.href='<?=$loginUrl?>'" style="margin-bottom:9px;">페이스북으로 로그인</button>
 
 <?

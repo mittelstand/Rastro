774c5b9b015717018880d96fc8a17f553c882841
민유성 <ysmin@mittelstand.co.kr>
2015-03-09 13:12:42 +0900

    test

--- pwEmailAc.php
+++ pwEmailAc.php
@@ -2,6 +2,15 @@
 header("Content-type: text/html; charset=utf-8"); 
 $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/inc.php";
+$exp = explode("-",$_GET['code']);
+
+
+$db = new Dbcon();
+$db->table = "member";
+$db->field = "email = tempEmail , tempEmail = ''";
+$db->where = "idx='".$exp[1]."' and emailCode='".$exp[0]."'";
+$db->Update();
+
 
 
 

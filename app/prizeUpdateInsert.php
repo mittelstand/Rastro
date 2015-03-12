0d4f9bb1a382ebbb05cc223f92060d6d8b4e18bc
전우현 <jwh@mittelstand.co.kr>
2015-03-12 16:45:33 +0900

    수상기록

--- app/prizeUpdateInsert.php
+++ app/prizeUpdateInsert.php
@@ -6,6 +6,8 @@ include $dir."/inc/header/inc.php";
 $uploadDir = $dir."/file/";
 $fileName = $_POST['fName'];
 $uploadFile = $uploadDir.time().".jpg";
+$upfile = explode(".", $_FILES['uploadedfile']['name']);
+			$upfileExt = $upfile[1]; //이미지 이름의 확장자
 
 $db= new Dbcon();
 if($_POST['div']=="insert"){

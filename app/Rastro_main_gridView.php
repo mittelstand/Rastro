4c3742f2c53b00520917731a5233e454214cc498
전우현 <jwh@mittelstand.co.kr>
2015-05-22 14:25:40 +0900

    11

--- app/Rastro_main_gridView.php
+++ app/Rastro_main_gridView.php
@@ -3,7 +3,7 @@ $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/inc.php";
 $db = new Dbcon();
 $db->table = "personalList";
-$db->field = "(select Count(idx) from personalList) as a";
+$db->field = "(select count(idx) from personalList) as a";
 $db->where = "type='sc'";
 $sel = $db->Select();
 $row =mysql_fetch_array($sel);

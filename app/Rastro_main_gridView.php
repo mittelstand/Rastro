d276bc63b0841938e0194c4fe2670b0d8ff4b735
전우현 <jwh@mittelstand.co.kr>
2015-05-22 14:39:44 +0900

    11

--- app/Rastro_main_gridView.php
+++ app/Rastro_main_gridView.php
@@ -2,11 +2,34 @@
 $dir = $_SERVER["DOCUMENT_ROOT"];
 include $dir."/inc/header/inc.php";
 $db = new Dbcon();
+
 $db->table = "personalList";
 $db->where = "type='sc'";
 $cnt=$db->TotalCnt();
-echo $cnt;
+$val['sc']= $cnt;
 
+$db->where = "type='ca'";
+$cnt=$db->TotalCnt();
+$val['ca']= $cnt;
+
+$db->where = "type='tr";
+$cnt=$db->TotalCnt();
+$val['tr']= $cnt;
+
+$db->where = "type='qu'";
+$cnt=$db->TotalCnt();
+$val['qu']= $cnt;
+
+$db->where = "type='cp'";
+$cnt=$db->TotalCnt();
+$val['cp']= $cnt;
+
+$db->where = "type='se'";
+$cnt=$db->TotalCnt();
+$val['se']= $cnt;
+
+
+echo json_encode($val);
 
 
 
